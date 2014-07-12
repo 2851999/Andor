4dff2507711cd010fec08d3fd7818d92efd41c50
Joel Davies <joelvdavies@btinternet.com>
2014-07-12 19:50:58 +0100

    Release 0 | Beta 0 | Development 0 | Experimental 1
    * Added Settings
    * Added Window
    * Added WindowTest
    
    * Created the package org.andor.core
    * Created the package org.andor.tests

---
+++ Andor/src/org/andor/tests/WindowTest.java
@@ -0,0 +1,19 @@
+package org.andor.tests;
+
+import org.andor.core.Window;
+
+public class WindowTest {
+	
+	/* The constructor */
+	public WindowTest() {
+		//Create the window
+		Window.create();
+	}
+	
+	/* The main method */
+	public static void main(String[] args) {
+		//Create a new instance of this test
+		new WindowTest();
+	}
+	
+}
